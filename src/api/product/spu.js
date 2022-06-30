c5c5226944483ee615d2dd9f8cb0c0f527dd2bdd
Zilong-417 <2627973708@qq.com>
2022-06-30 22:04:29 +0800

    完善功能

--- src/api/product/spu.js
+++ src/api/product/spu.js
@@ -5,3 +5,28 @@ import request from '@/utils/request'
 export const reqSpuList = (page, limit, category3Id) => request({
     url: `/admin/product/${page}/${limit}`, method: 'get', params: { category3Id }
 })
+
+
+//获取SPU信息
+///admin/product/getSpuById/{spuId} get
+export const reqSpu = (spuId) => request({
+    url: `admin/product/getSpuById/${spuId}`, method: 'get'
+})
+
+//获取品牌信息
+///admin/product/baseTrademark/getTrademarkList  get
+export const reqTrademarkList = () => request({
+    url: `admin/product/baseTrademark/getTrademarkList`, method: 'get'
+})
+
+//获取SPU图片的接口
+///admin/product/spuImageList/{spuId} get
+export const reqSpuImageList = (spuId) => request({
+    url: `admin/product/spuImageList/${spuId}`, method: 'get'
+})
+
+//获取平台全部销售属性 ----整个平台销售属性一共三个
+///admin/product/baseSaleAttrList
+export const reqSaleAttrList = () => request({
+    url: `admin/product/baseSaleAttrList`, method: 'get'
+})
--- src/views/product/Spu/SpuForm/index.vue
+++ src/views/product/Spu/SpuForm/index.vue
@@ -1,20 +1,21 @@
 <template>
     <div>
-        <el-form label-width="80px" ref="form">
+        <el-form label-width="80px" ref="form" :model="spu">
             <el-form-item label="Spu名称">
-                <el-input placeholder="请输入Spu名称"></el-input>
+                <el-input placeholder="请输入Spu名称" v-model="spu.spuName"></el-input>
             </el-form-item>
             <el-form-item label="品牌">
-                <el-select placeholder="请选择品牌" value="">
-                    <el-option :label="1" value="value"></el-option>
+                <el-select placeholder="请选择品牌" v-model="spu.tmId">
+                    <el-option :label="tm.tmName" :value="tm.id" v-for="(tm, index) in  trademarkList" :key="tm.id">
+                    </el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="SPU描述">
-                <el-input type="textarea" placeholder="请输入SPU描述" rows="4"></el-input>
+                <el-input type="textarea" placeholder="请输入SPU描述" rows="4" v-model="spu.description"></el-input>
             </el-form-item>
             <el-form-item label="SPU图片">
-                <el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card"
-                    :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+                <el-upload action="/dev-api/admin/product/fileUpload" list-type="picture-card"
+                    :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :file-list="spuImageList">
                     <i class="el-icon-plus"></i>
                 </el-upload>
                 <el-dialog :visible.sync="dialogVisible">
@@ -47,7 +48,43 @@ export default {
     data() {
         return {
             dialogImageUrl: '',
-            dialogVisible: false
+            dialogVisible: false,
+            //spu初始化为一个空对象
+            spu: {
+                "category3Id": 0,
+                "description": "string",
+                "spuImageList": [
+                    // {
+                    //     "id": 0,
+                    //     "imgName": "string",
+                    //     "imgUrl": "string",
+                    //     "spuId": 0
+                    // }
+                ],
+                "spuName": "string",
+                "tmId": 0,
+                "spuSaleAttrList": [
+                    // {
+                    //     "baseSaleAttrId": 0,
+                    //     "id": 0,
+                    //     "saleAttrName": "string",
+                    //     "spuId": 0,
+                    //     "spuSaleAttrValueList": [
+                    //         {
+                    //             "baseSaleAttrId": 0,
+                    //             "id": 0,
+                    //             "isChecked": "string",
+                    //             "saleAttrName": "string",
+                    //             "saleAttrValueName": "string",
+                    //             "spuId": 0
+                    //         }
+                    //     ]
+                    // }
+                ],
+            },//存储spu信息属性
+            trademarkList: [],//品牌信息
+            spuImageList: [],//存spu图片
+            saleAttrList: [],//销售属性的数据
         }
     },
     methods: {
@@ -59,8 +96,35 @@ export default {
             this.dialogVisible = true;
         },
         //初始化SpuForm数据
-        initSpuData(spu) {
-            console.log('发请求', spu)
+        async initSpuData(spu) {
+            //获取spu信息的数据
+            let SpuResult = await this.$API.spu.reqSpu(spu.id)
+            console.log(SpuResult)
+            if (SpuResult.code == 200) {
+                this.spu = SpuResult.data
+            }
+            //获取品牌信息
+            let trademarkResult = await this.$API.spu.reqTrademarkList();
+            if (trademarkResult.code == 200) {
+                this.trademarkList = trademarkResult.data
+            }
+            //获取SPU图片
+            let spuImageResult = await this.$API.spu.reqSpuImageList(spu.id);
+            if (spuImageResult.code == 200) {
+                let listArr = spuImageResult.data;
+                //由于照片墙想爱你是图片需要数组，数组里面要有name与url
+                //需要把服务器返回的数据进行修改
+                listArr.forEach(item => {
+                    item.name = item.imgName;
+                    item.url = item.imgUrl
+                })
+                this.spuImageList = listArr
+            }
+            //获取平台全部的销售属性
+            let saleResult = await this.$API.spu.reqSaleAttrList()
+            if (saleResult.code == 200) {
+                this.saleAttrList = saleResult.data
+            }
         }
     },
 
